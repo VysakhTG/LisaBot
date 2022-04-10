a353fea4600daef02d076a7998266f0c3eebc00e
Tony Stark <101032366+Tony-stark003@users.noreply.github.com>
2022-04-10 12:54:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,6 +34,9 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # This is required for the plugins involving the file system.
 TMP_DOWNLOAD_DIRECTORY = environ.get("TMP_DOWNLOAD_DIRECTORY", "./DOWNLOADS/")
 
+# Command
+COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
+
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
