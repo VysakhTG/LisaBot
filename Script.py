a8f7c39b68856fa27aaf0e319d13f7780f3ce7b2
Tony Stark <101032366+Tony-stark003@users.noreply.github.com>
2022-04-10 07:00:51 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -18,7 +18,7 @@ class script(object):
 - <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
+- Filter is the feature were users can set automated replies for a particular keyword and 𝙻𝚒𝚜𝚊 will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
 1. 𝙻𝚒𝚜𝚊 should have admin privillage.
