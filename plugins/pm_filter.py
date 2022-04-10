961e4af6d2e81cd2c41b38c9a011d611d8c1251f
Tony Stark <101032366+Tony-stark003@users.noreply.github.com>
2022-04-10 09:40:30 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -500,7 +500,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.TELE_TXT 
+            text=script.TELE_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
