95facb3cfae5f4097059a0fd2525a57c4aa6d984
Tony Stark <101032366+Tony-stark003@users.noreply.github.com>
2022-04-11 15:48:20 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -66,7 +66,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = '©️𝙿𝙾𝚆𝙴𝚁𝙴𝙳 𝙱𝚈 ›› [muѕíc вσч](https://t.me/M_autofilter_bot)'
+        rep = '©️ ᴘᴏwᴇʀᴇᴅ ʙʏ ›› [muѕíc вσч](https://t.me/M_autofilter_bot)'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
