3c1f588c95b908a0d857415c15342d5152e0de6b
Tony Stark <101032366+Tony-stark003@users.noreply.github.com>
2022-04-12 14:08:48 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -503,6 +503,17 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
+    elif query.data == "restric":
+        buttons = [[
+            InlineKeyboardButton('𝙱𝙰𝙲𝙺', callback_data='help')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.RESTRIC_TXT, 
+            disable_web_page_preview=True,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )
     elif query.data == "ytthumb":
         buttons = [[
             InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='help')
