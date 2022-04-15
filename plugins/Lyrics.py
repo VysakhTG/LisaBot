641e919d6c9b594f8e4659dc0a677423bb09e85a
Tony Stark <101032366+Tony-stark003@users.noreply.github.com>
2022-04-15 12:55:11 +0530

    Add files via upload

---
+++ plugins/Lyrics.py
@@ -0,0 +1,38 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
+
+import requests 
+
+import os
+
+
+API = "https://apis.xditya.me/lyrics?song="
+
+@Client.on_message(filters.command("lyrics"))
+async def lyrics(client, message):
+    searchlyrics = message.text.split(" ", 1)
+    if len(searchlyrics) == 1:
+                await message.reply_text("**Usage:**\n/lyrics [lyrics]")
+                return
+          else:
+               searchlyrics = searchip[1]
+               m = await message.reply_text("Searching...")
+          try:
+            await mee.delete()
+            await bot.send_message(chat_id, text = rpl, reply_to_message_id = message.message_id, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs ", url = f"t.me/xd_botz")]]))
+          except Exception as e:                            
+             await message.reply_text(f"I Can't Find A Song With `{song}`", quote = True, reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url = f"t.me/xd_botz")]]))
+
+
+
+def search(song):
+        r = requests.get(API + song)
+        find = r.json()
+        return find
+       
+def lyrics(song):
+        fin = search(song)
+        text = f'**🎶 Successfully Extracted Lyrics Of {song} 🎶**\n\n'
+        text += f'`{fin["lyrics"]}`'
+        text += '\n\n\n**Made By Artificial Intelligence**'
+        return text
