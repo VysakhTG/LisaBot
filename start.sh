648d1645fc9c1ef3693db7639a52fc95109d4f3e
Tony Stark <101032366+Tony-stark003@users.noreply.github.com>
2022-04-18 05:16:25 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /LisaBot
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
