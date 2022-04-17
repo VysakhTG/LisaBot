1d796e9b3e74583b01370957895317031c6e1a7f
Tony Stark <101032366+Tony-stark003@users.noreply.github.com>
2022-04-17 06:25:25 +0530

    Update __init__.py

--- Alita/__init__.py
+++ Alita/__init__.py
@@ -50,7 +50,7 @@ except Exception as ef:
     sysexit(1)
 
 LOGGER.info("------------------------")
-LOGGER.info("|      Tɪɢᴇʀ Sʜʀᴏғғ     |")
+LOGGER.info("|         𝙻𝚒𝚜𝚊         |")
 LOGGER.info("------------------------")
 LOGGER.info(f"Version: {Config.VERSION}")
 LOGGER.info(f"Owner: {str(Config.OWNER_ID)}")
